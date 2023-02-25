f6548f934df4342a8ccd05817b82b5b03ff76f06
JordanWitt <wittjordan94@gmail.com>
2023-02-24 18:41:55 -0600

    working on pw match

--- JS/js_practice.js
+++ JS/js_practice.js
@@ -5,13 +5,23 @@ let noVowels = name.replace(/[aeiou]/gi, '')
 console.log(noVowels);
 
 let username = "jordierwitt"
-let password = "helloiamjordan"
+let password = "HelloIamJordan!@"
 
 function passwordCheck() {
     if (password.length < 10) {
         console.log("sorry, password not long enough");
-    } else {
+    }
+    else if(password === username){
+        console.log("sorry you cannot have that as a password");
+    }
+    else if(password.match(/[a-zA-Z]/g).length < 20)
+    {
+        console.log("sorry not unique");
+    }
+    else {
         console.log("great password");
     }
 }
 passwordCheck();
+
+
