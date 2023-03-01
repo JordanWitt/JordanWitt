a40d083723c2f98c4a3ecbc7b3668ed0a9a8c42d
JordanWitt <wittjordan94@gmail.com>
2023-02-28 22:24:10 -0600

    adding upper and lowercase validation

--- JS/pwverification.js
+++ JS/pwverification.js
@@ -8,3 +8,31 @@ myInput.onfocus = function (){
     document.getElementById("message").style.display = "block";
 }
 // shows this field shows the message from the HTML on the PW sections
+myInput.onblur = function (){
+    document.getElementById("message").style.display = "none"
+}
+// hides the message box when user clicks away
+myInput.onkeyup = function () {
+    //validation for letters
+    let lowerCase = /[a-z]/g
+    if(myInput.value.match(lowerCase)){
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    }else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+
+    //validate capital letters
+    let upperCase = /[A-Z]/g
+    if(myInput.value.match(upperCase)){
+        letter.classList.remove("invalid");
+        letter.classList.add("valid");
+    }else {
+        letter.classList.remove("valid");
+        letter.classList.add("invalid");
+    }
+
+    //validate numbers
+}
