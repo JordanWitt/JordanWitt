2ab57e7381e2fc0cdc7b562b1c8fdd2786aae842
JordanWitt <wittjordan94@gmail.com>
2023-04-12 15:15:11 -0500

    adding api

--- js/landing.js
+++ js/landing.js
@@ -16,6 +16,8 @@ let cursor = document.getElementById("circularCursor");
 let logoTxt = document.getElementById("logo-text");
 let loginBtn = document.getElementById("login");
 let contactBtn = document.getElementById("contact");
+let signup = document.getElementById("signup");
+let slogan = document.getElementById("p-slogan");
 let darkMode = false;
 
 moon.addEventListener("click", function () {
@@ -28,6 +30,8 @@ moon.addEventListener("click", function () {
         logoTxt.classList.add("logo-text-dark");
         contactBtn.classList.add("contact-dark", "contact-dark:hover");
         loginBtn.classList.add("login-dark", "login-dark:hover");
+        signup.classList.add("signup-dark", "signup-dark:hover");
+        slogan.classList.add("slogan-dark");
 
     } else {
         body.classList.remove("dark-mode");
@@ -37,5 +41,7 @@ moon.addEventListener("click", function () {
         logoTxt.classList.remove("logo-text-dark");
         contactBtn.classList.remove("contact-dark", "contact-dark:hover");
         loginBtn.classList.remove("login-dark", "login-dark:hover");
+        signup.classList.remove("signup-dark", "signup-dark:hover");
+        slogan.classList.remove("slogan-dark");
     }
 })
